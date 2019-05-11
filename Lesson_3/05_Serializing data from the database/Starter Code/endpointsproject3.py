2f456c05cd86efe48dbd1bf2b1cd37e720de3281
Talon Jones <taloncjones@gmail.com>
2019-05-11 23:46:57 +0000

    Cleaned up starter code

--- Lesson_3/05_Serializing data from the database/Starter Code/endpointsproject3.py
+++ Lesson_3/05_Serializing data from the database/Starter Code/endpointsproject3.py
@@ -10,9 +10,9 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-# Create the appropriate app.route functions, 
+# Create the appropriate app.route functions,
 #test and see if they work
 
 
@@ -26,29 +26,29 @@ def puppiesFunction():
   elif request.method == 'POST':
     #Call the method to make a new puppy
     print "Making a New puppy"
-    
+
     name = request.args.get('name', '')
     description = request.args.get('description', '')
     print name
     print description
     return makeANewPuppy(name, description)
- 
-  
- 
+
+
+
 #Make another app.route() decorator here that takes in an integer id in the URI
 @app.route("/puppies/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
 #Call the method to view a specific puppy
 def puppiesFunctionId(id):
   if request.method == 'GET':
     return getPuppy(id)
-    
-#Call the method to edit a specific puppy  
+
+#Call the method to edit a specific puppy
   elif request.method == 'PUT':
     name = request.args.get('name', '')
     description = request.args.get('description', '')
     return updatePuppy(id,name, description)
-    
- #Call the method to remove a puppy 
+
+#Call the method to remove a puppy
   elif request.method == 'DELETE':
     return deletePuppy(id)
 
@@ -58,8 +58,8 @@ def getAllPuppies():
 
 def getPuppy(id):
   puppy = session.query(Puppy).filter_by(id = id).one()
-  return jsonify(puppy=puppy.serialize) 
-  
+  return jsonify(puppy=puppy.serialize)
+
 def makeANewPuppy(name,description):
   puppy = Puppy(name = name, description = description)
   session.add(puppy)
@@ -85,4 +85,4 @@ def deletePuppy(id):
 
 if __name__ == '__main__':
     app.debug = False
-    app.run(host='0.0.0.0', port=5000)	
+    app.run(host='0.0.0.0', port=5000
