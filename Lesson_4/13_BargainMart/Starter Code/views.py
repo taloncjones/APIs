ed88c1a904bcc8747ad802380b5e3c0881c2435c
Talon Jones <taloncjones@gmail.com>
2019-05-12 22:46:12 +0000

    Add ratelimit method

--- Lesson_4/13_BargainMart/Starter Code/views.py
+++ Lesson_4/13_BargainMart/Starter Code/views.py
@@ -49,6 +49,19 @@ def get_view_rate_limit():
 def on_over_limit(limit):
     return (jsonify({'data':'You hit the rate limit', 'error':'429'}), 429)
 
+def ratelimit(limit, per=300, send_x_headers=True, over_limit=on_over_limit,
+              scope_func=lambda: request.remote_addr,
+              key_func=lambda: request.endpoint):
+    def decorator(f):
+        def rate_limited(*args, **kwargs):
+            key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
+            rlimit = RateLimit(key, limit, per, send_x_headers)
+            g._view_rate_limit = rlimit
+            if over_limit is not None and rlimit.over_limit:
+                return over_limit(rlimit)
+            return f(*args, **kwargs)
+        return update_wrapper(rate_limited, f)
+    return decorator
 
 @app.route('/catalog')
 def getCatalog():
