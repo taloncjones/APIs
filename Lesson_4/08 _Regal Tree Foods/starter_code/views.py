df7fad3c18d8e5b436a233bb03fea91b05ded130
Talon Jones <taloncjones@gmail.com>
2019-05-12 03:30:34 +0000

    Add route/method to get auth token

--- Lesson_4/08 _Regal Tree Foods/starter_code/views.py
+++ Lesson_4/08 _Regal Tree Foods/starter_code/views.py
@@ -32,7 +32,11 @@ def verify_password(username_or_token, password):
     return True
 
 #add /token route here to get a token for a user with login credentials
-
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
 
 
 
