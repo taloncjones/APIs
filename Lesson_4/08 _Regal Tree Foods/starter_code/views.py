bede8f2ee0cbe31b7815f855b14a3222a5cdf7b6
Talon Jones <taloncjones@gmail.com>
2019-05-12 03:34:37 +0000

    Fix thread issue

--- Lesson_4/08 _Regal Tree Foods/starter_code/views.py
+++ Lesson_4/08 _Regal Tree Foods/starter_code/views.py
@@ -8,7 +8,7 @@ from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 
 
-engine = create_engine('sqlite:///regalTree.db')
+engine = create_engine('sqlite:///regalTree.db', connect_args={'check_same_thread':False})
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
